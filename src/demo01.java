bb832792a2fc8c646251e23afba02fc21e8c2f7d
wangchoa <18107024178@163.com>
2019-01-20 08:45:52 +0800

    ooooooooo

---
+++ src/demo01.java
@@ -0,0 +1,11 @@
+public class demo01 {
+/**
+ * @Description: java类作用描述
+ * @Author: wangchao
+ * @CreateDate: 2019/1/20 8:44
+ * @Version: 1.0
+ */
+public static void main(String[] args) {
+    System.out.println("这是测试");
+}
+}
